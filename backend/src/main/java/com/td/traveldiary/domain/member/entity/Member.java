c0a446adbdaaf7eb71f7005ef686b24f5f429f53
yooshnn <yooshnn@kakao.com>
2026-04-11 14:27:45 +0900

    feat: add getter and profileImageUrl field to Member entity

--- backend/src/main/java/com/td/traveldiary/domain/member/entity/Member.java
+++ backend/src/main/java/com/td/traveldiary/domain/member/entity/Member.java
@@ -2,20 +2,26 @@ package com.td.traveldiary.domain.member.entity;
 
 import com.td.traveldiary.global.entity.BaseEntity;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+@Getter
+@NoArgsConstructor
 public class Member extends BaseEntity {
     private Long id;
     private String name;
+    private String profileImageUrl;
     private String providerId;
     private Provider provider;
     private Role role;
     private boolean isDeleted;
 
     @Builder
-    public Member(Long id, String name, String providerId, Provider provider, Role role, boolean isDeleted) {
+    public Member(Long id, String name, String profileImageUrl, String providerId, Provider provider, Role role, boolean isDeleted) {
         this.id = id;
         this.name = name;
+        this.profileImageUrl = profileImageUrl;
         this.providerId = providerId;
         this.provider = provider;
         this.role = role;
