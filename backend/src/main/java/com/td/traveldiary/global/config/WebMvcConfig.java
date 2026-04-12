fc13a9bf8d969e95a23574340e0a0fe82e2e6b46
yooshnn <yooshnn@kakao.com>
2026-04-12 10:23:25 +0900

    feat: add @CurrentMemberId annotation and ArgumentResolver

---
+++ backend/src/main/java/com/td/traveldiary/global/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.td.traveldiary.global.config;
+
+import com.td.traveldiary.global.resolver.CurrentMemberIdArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final CurrentMemberIdArgumentResolver currentMemberIdArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(currentMemberIdArgumentResolver);
+    }
+}
